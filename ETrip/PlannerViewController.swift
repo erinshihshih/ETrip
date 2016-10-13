2aac5100838fc6719f4db6d2c4ad2f609aab394d
erinshihshih <erinshih88@gmail.com>
2016-10-13 17:20:24 +0800

    refactor part 5

--- ETrip/PlannerViewController.swift
+++ ETrip/PlannerViewController.swift
@@ -10,18 +10,18 @@ import UIKit
 
 class PlannerViewController: UITabBarController {
   
-    var post: Post?
-    var posts: [Post] = []
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var countryLabel: UILabel!
-    @IBOutlet weak var startDateLabel: UILabel!
-    @IBOutlet weak var returnDateLabel: UILabel!
+//    var post: Post?
+//    var posts: [Post] = []
+//    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var countryLabel: UILabel!
+//    @IBOutlet weak var startDateLabel: UILabel!
+//    @IBOutlet weak var returnDateLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Firebase Manager Delegate
-        FirebaseManager.shared.delegate = self
+//        FirebaseManager.shared.delegate = self
 //        FirebaseManager.shared.fetchPosts()
         
 //        self.titleLabel.text = posts.description
@@ -38,20 +38,20 @@ class PlannerViewController: UITabBarController {
 
 
 }
-extension PlannerViewController: FirebaseManagerDelegate {
-    
-    func getPostManager(getPostManager: FirebaseManager, didGetData post: Post) {
-        
-//        self.posts.append(post)
-    
-    }
-    
-    func getTransportationManager(getTransportationManager: FirebaseManager, didGetData transportation: Transportation) {
-        
-    }
-    
-    func getAttractionManager(getAttractionManager: FirebaseManager, didGetData attraction: Attraction) {
-        
-    }
-    
-}
+//extension PlannerViewController: FirebaseManagerDelegate {
+//    
+//    func getPostManager(getPostManager: FirebaseManager, didGetData post: Post) {
+//        
+////        self.posts.append(post)
+//    
+//    }
+//    
+//    func getTransportationManager(getTransportationManager: FirebaseManager, didGetData transportation: Transportation) {
+//        
+//    }
+//    
+//    func getAttractionManager(getAttractionManager: FirebaseManager, didGetData attraction: Attraction) {
+//        
+//    }
+//    
+//}
