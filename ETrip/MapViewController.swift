73cef9a347ddba7276b8bf5b41a44c62e09a16bc
erinshihshih <erinshih88@gmail.com>
2016-10-19 16:40:39 +0800

    google place picker

--- ETrip/MapViewController.swift
+++ ETrip/MapViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlacePicker
 
-class MapViewController: UIViewController, UISearchBarDelegate {
+class MapViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate {
     
     var post: Post?
     var transportation: Transportation?
@@ -22,44 +23,121 @@ class MapViewController: UIViewController, UISearchBarDelegate {
     
     @IBOutlet weak var editButton: UIButton!
     
-  
     @IBOutlet weak var mapView: GMSMapView!
     
+    var locationManager: CLLocationManager!
+    var placePicker: GMSPlacePicker!
+    var latitude: Double!
+    var longitude: Double!
+    
     @IBAction func searchWithAddress(sender: AnyObject) {
-        let serchController = UISearchController(searchResultsController: nil)
-        serchController.searchBar.delegate = self
-        self.presentViewController(serchController, animated: true, completion: nil)
+//        let serchController = UISearchController(searchResultsController: nil)
+//        serchController.searchBar.delegate = self
+//        self.presentViewController(serchController, animated: true, completion: nil)
+        let center = CLLocationCoordinate2DMake(51.5108396, -0.0922251)
+        let northEast = CLLocationCoordinate2DMake(center.latitude + 0.001, center.longitude + 0.001)
+        let southWest = CLLocationCoordinate2DMake(center.latitude - 0.001, center.longitude - 0.001)
+        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+        let config = GMSPlacePickerConfig(viewport: viewport)
+        placePicker = GMSPlacePicker(config: config)
         
+        placePicker?.pickPlaceWithCallback({ (place: GMSPlace?, error: NSError?) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let place = place {
+                print("Place name \(place.name)")
+                print("Place address \(place.formattedAddress)")
+                print("Place attributions \(place.attributions)")
+            } else {
+                print("No place selected")
+            }
+        })
+        
+//        // 1
+//        let center = CLLocationCoordinate2DMake(self.latitude, self.longitude)
+//        let northEast = CLLocationCoordinate2DMake(center.latitude + 0.001, center.longitude + 0.001)
+//        let southWest = CLLocationCoordinate2DMake(center.latitude - 0.001, center.longitude - 0.001)
+//        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+//        let config = GMSPlacePickerConfig(viewport: viewport)
+//        self.placePicker = GMSPlacePicker(config: config)
+//        
+//        // 2
+//        placePicker.pickPlaceWithCallback { (place: GMSPlace?, error: NSError?) -> Void in
+//            
+//            if let error = error {
+//                print("Error occurred: \(error.localizedDescription)")
+//                return
+//            }
+//            // 3
+//            if let place = place {
+//                let coordinates = CLLocationCoordinate2DMake(place.coordinate.latitude, place.coordinate.longitude)
+//                let marker = GMSMarker(position: coordinates)
+//                marker.title = place.name
+//                marker.map = self.mapView
+//                self.mapView.animateToLocation(coordinates)
+//            } else {
+//                print("No place was selected")
+//            }
+//        }
+
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        
         
         self.titleLabel.text = post?.title
         self.countryLabel.text = post?.country
         self.startDateLabel.text = post?.startDate
         self.returnDateLabel.text = post?.returnDate
+        
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
 
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(true)
         
+//        let camera = GMSCameraPosition.cameraWithLatitude(self.latitude, longitude: self.longitude, zoom: 18)
+//        mapView.myLocationEnabled = true
+//        mapView.camera = camera
         
-        let camera = GMSCameraPosition.cameraWithLatitude(25.042789, longitude: 121.564869, zoom: 18)
-        mapView.myLocationEnabled = true
-        mapView.camera = camera
-        
-        let currentLocation = CLLocationCoordinate2DMake(25.042789, 121.564869)
-        let marker = GMSMarker(position: currentLocation)
-        marker.title = "AppWorks"
-        marker.snippet = "Taipei"
-        marker.map = mapView
+//        let currentLocation = CLLocationCoordinate2DMake(25.042789, 121.564869)
+//        let marker = GMSMarker(position: currentLocation)
+//        marker.title = "AppWorks"
+//        marker.snippet = "Taipei"
+//        marker.map = mapView
         
     }
+    
+    //MARK: Location protocol
+    func locationManager(manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]){
+        // 1
+        let location:CLLocation = locations.last!
+        self.latitude = location.coordinate.latitude
+        self.longitude = location.coordinate.longitude
+        
+        // 2
+        let coordinates = CLLocationCoordinate2DMake(self.latitude, self.longitude)
+        let marker = GMSMarker(position: coordinates)
+        marker.title = "I am here"
+        marker.map = self.mapView
+        self.mapView.animateToLocation(coordinates)
+    }
+    
+    func locationManager(manager: CLLocationManager,
+                         didFailWithError error: NSError){
+        
+        print("An error occurred while tracking location changes : \(error.description)")
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
